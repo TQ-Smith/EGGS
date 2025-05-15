a1789277924908b47f94fc932ecdb2be250a6f43
TQ-Smith <thomasqsmith2000@gmail.com>
2025-05-15 12:50:35 -0400

    Working on evaluation ...

--- commands.bash
+++
@@ -1,5 +0,0 @@
-convertf -p param.txt
-plink --file zeng2025 --recode vcf --out zeng2025
-bgzip zeng2025.vcf
-tabix zeng2025.vcf.gz
-bcftools view -r 22 -e 'COUNT(GT="mis")=N_SAMPLES' zeng2025.vcf.gz | bcftools view -e 'N_ALT=0' | gzip > zeng2025_chr22.vcf.gz
